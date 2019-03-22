1824ff194d3aad8dbcaa9391f5aae1ef4dc86bfc
jdman495 <48835968+jdman495@users.noreply.github.com>
2019-03-23 01:35:21 +0900

    test

--- 01/index.php
+++ 01/index.php
@@ -1,6 +1,4 @@
-
 <?php
-// This results in an error.
-// The output above is before the header() call
-header('Location: https://i.imgur.com/bD3Nn6x.gif');
+header('Location: https://i.imgur.com/3lwSbBr.png');
+
 ?>
